04aa3f01a180d849f12cd83f6af8df0bde956533
Leandro Marim <leandro.marim@hotmail.com>
2018-05-04 15:39:16 -0300

    Adicionando o template ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ImportsComponent } from './imports/imports.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    ImportsComponent
   ],
   imports: [
     BrowserModule
