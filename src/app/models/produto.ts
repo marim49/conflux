2fe5fe4986eb6f188e49f700caa94405170c878d
unknown <pablonovosrumos@hotmail.com>
2018-06-11 18:01:22 -0300

    cadastro de produtos gravando no banco e mostrando na tela

---
+++ src/app/models/produto.ts
@@ -0,0 +1,9 @@
+export class produto{
+    $key:any;
+    NomeProduto:string;
+    UnidadeMedida: String;
+    QuantidadePadrao: string;
+    Valor: number;
+    Status:string;   
+
+}
--- src/app/produto/cadastro-produto/cadastro-produto.component.ts
+++ src/app/produto/cadastro-produto/cadastro-produto.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormsModule,NgForm} from '@angular/forms';
+import {AngularFireDatabase} from 'angularfire2/database';
+import {AngularFireList} from 'angularfire2/database';
+import {Observable} from 'rxjs/Observable';
+import {produto} from '../../models/produto';
+
 
 @Component({
   selector: 'app-cadastro-produto',
@@ -6,10 +12,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro-produto.component.css']
 })
 export class CadastroProdutoComponent implements OnInit {
+  
+  listaprodutos:AngularFireList<any>;
+  opstatus:any;
+  produtos: Observable<any[]>;
+  
 
-  constructor() { }
+  
+  constructor(private angularfire:AngularFireDatabase) { }
 
   ngOnInit() {
+    this.produtos=this.angularfire.list("Produtos").valueChanges();
+    
   }
+  //metodo recebe os dados do formulario e grava no firebase
+  gravaproduto(cadastroproduto:NgForm)
+  {
+    this.angularfire.list("Produtos").push({
+    NomeProduto:cadastroproduto.form.controls.nomeproduto.value,
+    UnidadeMedida: cadastroproduto.form.controls.unidademedida.value,
+    QuantidadePadrao: cadastroproduto.form.controls.qtdpadrao.value,
+    Valor: cadastroproduto.form.controls.valor.value,
+    Status:this.opstatus,
+  })
+
+    cadastroproduto.controls.nomeproduto.setValue("");
+    cadastroproduto.controls.unidademedida.setValue("");
+    cadastroproduto.controls.valor.setValue("");
+    cadastroproduto.controls.qtdpadrao.setValue("");
+  }
+  
+  
+  getProdutos(){
+   
+  }
+  updateprodutos(){
+    
+  }
+  resetform(cadastroproduto:NgForm){
+    if(cadastroproduto!=null){
+
+    }
+  }
+
+  
 
 }
