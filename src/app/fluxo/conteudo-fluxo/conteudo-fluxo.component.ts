afc16cf12f38e5d2544eefe735dc54bb8486bd1e
Leandro Marim <leandro.marim@hotmail.com>
2018-07-04 14:36:44 -0300

    add clientes fluxo

--- src/app/fluxo/conteudo-fluxo/conteudo-fluxo.component.ts
+++ src/app/fluxo/conteudo-fluxo/conteudo-fluxo.component.ts
@@ -1,3 +1,4 @@
+import {FormsModule, NgForm} from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import {AngularFireDatabase} from 'angularfire2/database';
 import {ToastrService} from 'ngx-toastr';
@@ -12,7 +13,10 @@ import { map } from 'rxjs/operators';
 })
 export class ConteudoFluxoComponent implements OnInit {
   listaprodutos: AngularFireList<any>;
+  listaclientes: AngularFireList<any>;
+  product:Array<any>;
   produtos: Observable<any[]>;
+  clientes: Observable<any[]>;
   CodProduto:any;
   CodCliente:any;
   constructor(
@@ -21,12 +25,20 @@ export class ConteudoFluxoComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.product = new Array<any>();
     this.listaprodutos = this.angularfire.list('Produtos');
+    this.listaclientes = this.angularfire.list('Clientes');
 
     this.produtos = this.listaprodutos.snapshotChanges().pipe(map(changes => changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))));
-
+    this.clientes = this.listaclientes.snapshotChanges().pipe(map(changes => changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))));
+    
 
   }
+  form_submit(g: NgForm){
+    console.log(g.form.controls);
+    console.log('produto: ' + g.form.controls.product.value);
+  }
+
   recebecodigo(evento: KeyboardEvent){
     this.CodProduto=(<HTMLInputElement>evento.target).value;
     console.log(this.CodProduto);
